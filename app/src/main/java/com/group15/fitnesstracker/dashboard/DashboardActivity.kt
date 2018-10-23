42a82eb15c7077d1377d3fab396f66ca01178b34
awave1 <artgolovin96@gmail.com>
2018-10-23 00:28:14 -0600

    simple dashboard with three pages

--- app/src/main/java/com/group15/fitnesstracker/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/group15/fitnesstracker/dashboard/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.group15.fitnesstracker.dashboard
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.group15.fitnesstracker.R
+import com.group15.fitnesstracker.dashboard.schedule.ScheduleFragment
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity: AppCompatActivity() {
