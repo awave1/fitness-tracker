8553f3d3d6d3e6be51e546d5ffc334f981904cf0
awave1 <artgolovin96@gmail.com>
2018-10-23 01:41:02 -0600

    check if onboarding happened

--- app/src/main/java/com/group15/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/group15/fitnesstracker/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.group15.fitnesstracker
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.group15.fitnesstracker.dashboard.DashboardFragment
+import com.group15.fitnesstracker.onboarding.OnboardingActivity
+import com.group15.fitnesstracker.util.Constants
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.container, DashboardFragment.instance)
-                .addToBackStack(null)
-                .commit()
+        val pref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+
+        if (!pref.getBoolean(Constants.USER_FIRST_TIME, true)) {
+            setContentView(R.layout.activity_main)
+            setSupportActionBar(toolbar)
+
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, DashboardFragment.instance)
+                    .addToBackStack(null)
+                    .commit()
+        } else {
+            startActivity(Intent(this, OnboardingActivity::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/group15/fitnesstracker/onboarding/OnboardingPageFragment.kt
+++ app/src/main/java/com/group15/fitnesstracker/onboarding/OnboardingPageFragment.kt
@@ -1,5 +1,6 @@
 package com.group15.fitnesstracker.onboarding
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +10,6 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.group15.fitnesstracker.MainActivity
 import com.group15.fitnesstracker.R
-import com.group15.fitnesstracker.dashboard.DashboardFragment
 import com.group15.fitnesstracker.util.Constants
 import kotlinx.android.synthetic.main.fragment_onboarding_page.*
 
@@ -28,6 +28,8 @@ class OnboardingPageFragment : Fragment(), OnboardingPageContract.View {
         presenter.showPage(pagePosition!!)
 
         onboardingStartBtn.setOnClickListener {
+            val sharedPref = context?.getSharedPreferences(context?.resources?.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            sharedPref?.edit()?.putBoolean(Constants.USER_FIRST_TIME, false)?.apply()
             startActivity(Intent(context, MainActivity::class.java))
             activity?.finish()
         }
