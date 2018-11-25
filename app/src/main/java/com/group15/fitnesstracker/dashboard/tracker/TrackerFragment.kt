a9986138d28345247594415543dc1b429df4330c
漢字 <emulateking@gmail.com>
2018-11-24 18:45:34 -0700

    added tracker (body/nutrition tracker)
    - grst209 gang

---
+++ app/src/main/java/com/group15/fitnesstracker/dashboard/tracker/TrackerFragment.kt
@@ -0,0 +1,14 @@
+package com.group15.fitnesstracker.dashboard.tracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.group15.fitnesstracker.R
+
+class TrackerFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tracker_page, container, false)
+    }
+}
