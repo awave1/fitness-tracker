55c3f9461dfeb863071de1ece93ae837f0f8ee35
awave1 <artem.golovin@renoworks.com>
2018-11-28 02:06:19 -0700

    add FAB menu (using lib)

--- app/src/main/java/com/group15/fitnesstracker/dashboard/workout/WorkoutFragment.kt
+++ app/src/main/java/com/group15/fitnesstracker/dashboard/workout/WorkoutFragment.kt
@@ -4,11 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.group15.fitnesstracker.R
+import kotlinx.android.synthetic.main.fragment_workout_page.*
 
 class WorkoutFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_workout_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addExercise.setOnClickListener {
+            Toast.makeText(context, "add exercise", Toast.LENGTH_SHORT).show()
+        }
+
+        pickWorkout.setOnClickListener {
+            Toast.makeText(context, "pick workout", Toast.LENGTH_SHORT).show()
+        }
     }
 }
