f3b13024a5cd48bc026e376437a7a4e72aedaff3
dartuso <daniel.artuso1@gmail.com>
2018-12-02 14:59:28 -0700

    fix primary keys

--- app/src/main/java/com/group15/fitnesstracker/db/Records.java
+++ app/src/main/java/com/group15/fitnesstracker/db/Records.java
@@ -4,14 +4,11 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(foreignKeys = {@ForeignKey(entity = User.class,parentColumns = "id",childColumns = "userId")
+@Entity(primaryKeys = {"userId, recordId"},foreignKeys = {@ForeignKey(entity = User.class,parentColumns = "id",childColumns = "userId")
         ,@ForeignKey(entity = NutritionRecording.class,parentColumns = "recordingId",childColumns = "recordId")
         ,@ForeignKey(entity = BodyMeasureRecording.class,parentColumns = "recordingId",childColumns = "recordId")})
 public class Records {
-    @PrimaryKey
     public String userId;
 
-    @PrimaryKey
-//    @ForeignKey() right now link to both may have to change
     public String recordId;
 }
