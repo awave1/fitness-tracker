c2d11e776caf4fa0f38af4fc72209372a879c9c2
dartuso <daniel.artuso1@gmail.com>
2018-12-02 14:50:25 -0700

    added primary keys to schedule

--- app/src/main/java/com/group15/fitnesstracker/db/Schedule.java
+++ app/src/main/java/com/group15/fitnesstracker/db/Schedule.java
@@ -4,7 +4,7 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(foreignKeys = {@ForeignKey(entity = Workout.class,parentColumns = "workoutId",childColumns = "workoutId"),
+@Entity(primaryKeys = "workoutId, trainerId, userId, recordingId",foreignKeys = {@ForeignKey(entity = Workout.class,parentColumns = "workoutId",childColumns = "workoutId"),
 @ForeignKey(entity = Trainer.class,parentColumns = "trainerId",childColumns = "trainerId"),
 @ForeignKey(entity = User.class,parentColumns = "id",childColumns = "userId"),
 @ForeignKey(entity = Records.class,parentColumns = "recordId",childColumns = "recordingId")})
