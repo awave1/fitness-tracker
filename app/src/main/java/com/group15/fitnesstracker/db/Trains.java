fc74afd0d6e6e5c310ce1ead1a30be68fa7a025c
dartuso <daniel.artuso1@gmail.com>
2018-12-02 14:49:23 -0700

    added primary keys to trains

--- app/src/main/java/com/group15/fitnesstracker/db/Trains.java
+++ app/src/main/java/com/group15/fitnesstracker/db/Trains.java
@@ -3,7 +3,7 @@ package com.group15.fitnesstracker.db;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 
-@Entity(foreignKeys = {@ForeignKey(entity = Trainer.class,parentColumns = "trainerId",childColumns = "trainerId"),
+@Entity(primaryKeys = "trainerid, userid",foreignKeys = {@ForeignKey(entity = Trainer.class,parentColumns = "trainerId",childColumns = "trainerId"),
 @ForeignKey(entity = User.class,parentColumns = "id",childColumns = "userId")})
 public class Trains {
     public int trainerId;
