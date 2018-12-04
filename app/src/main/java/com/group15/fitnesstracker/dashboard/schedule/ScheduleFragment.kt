efe5635688054bdfbf980809356d4e2c0dc9c0ef
awave1 <artem.golovin@renoworks.com>
2018-12-03 23:45:36 -0700

    make schedule appear & stop crashing

--- app/src/main/java/com/group15/fitnesstracker/dashboard/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/group15/fitnesstracker/dashboard/schedule/ScheduleFragment.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.group15.fitnesstracker.R
+import kotlinx.android.synthetic.main.fragment_schedule_page.*
+import com.alamkanak.weekview.WeekViewDisplayable
+import com.alamkanak.weekview.MonthLoader
+import com.group15.fitnesstracker.R.id.weekView
+import java.util.*
+
 
 class ScheduleFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -14,7 +21,11 @@ class ScheduleFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        weekView.setOnEventClickListener { any, eventRect ->
+            //
+        }
+
+        weekView.setMonthChangeListener { startDate, endDate -> emptyList() }
     }
-
-
 }
---
+++ app/src/main/java/com/group15/fitnesstracker/dashboard/schedule/ScheduleItem.kt
@@ -0,0 +1,10 @@
+package com.group15.fitnesstracker.dashboard.schedule
+
+import com.alamkanak.weekview.WeekViewDisplayable
+import com.alamkanak.weekview.WeekViewEvent
+
+class ScheduleItem: WeekViewDisplayable<ScheduleItem> {
+    override fun toWeekViewEvent(): WeekViewEvent<ScheduleItem> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
