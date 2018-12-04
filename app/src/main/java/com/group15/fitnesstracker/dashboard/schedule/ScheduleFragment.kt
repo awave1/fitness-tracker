a79288e1dde5a24be69bbdedd150ddfcebd2fff6
awave1 <artem.golovin@renoworks.com>
2018-12-03 23:38:54 -0700

    add new weekview lib

--- app/src/main/java/com/group15/fitnesstracker/dashboard/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/group15/fitnesstracker/dashboard/schedule/ScheduleFragment.kt
@@ -1,16 +1,11 @@
 package com.group15.fitnesstracker.dashboard.schedule
 
-import android.graphics.RectF
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.alamkanak.weekview.MonthLoader
-import com.alamkanak.weekview.WeekView
-import com.alamkanak.weekview.WeekViewEvent
 import com.group15.fitnesstracker.R
-import kotlinx.android.synthetic.main.fragment_schedule_page.*
 
 class ScheduleFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -19,9 +14,6 @@ class ScheduleFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        weekView.setOnEventClickListener()
-        weekView.setMonthChangeListener()
-        weekView.setEventLongPressListener()
     }
 
 
