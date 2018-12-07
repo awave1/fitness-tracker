5b100ddf1134f755e48738a15d8cad378663b3ad
dartuso <daniel.artuso1@gmail.com>
2018-12-07 01:39:16 -0700

    idk what im doin but its betters?

--- app/src/main/java/com/group15/fitnesstracker/dashboard/profile/DailyAdapter.kt
+++ app/src/main/java/com/group15/fitnesstracker/dashboard/profile/DailyAdapter.kt
@@ -5,8 +5,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.group15.fitnesstracker.R
+import com.group15.fitnesstracker.R.layout.daily_item
 
-class DailyAdapter(val view: Double): RecyclerView.Adapter<DailyAdapter.DailyViewHolder>(){
+class DailyAdapter<T>(val view: Int): RecyclerView.Adapter<DailyAdapter.DailyViewHolder>(){
     class DailyViewHolder (val textView: TextView) : RecyclerView.ViewHolder(textView)
     var items = mutableListOf<T>()
         set(value) {
@@ -15,7 +16,7 @@ class DailyAdapter(val view: Double): RecyclerView.Adapter<DailyAdapter.DailyVie
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyViewHolder {
-        return DailyViewHolder(LayoutInflater.from(parent.context).inflate(view, parent, false))
+        return DailyViewHolder()
     }
 
     override fun onBindViewHolder(holder: DailyViewHolder, position: Int) {
@@ -25,6 +26,16 @@ class DailyAdapter(val view: Double): RecyclerView.Adapter<DailyAdapter.DailyVie
                 val protein = items[1] as Double
                 val carbohydrate = items[2] as Double
                 val fat = items[3] as Double
+
+                val cal = holder.itemView.findViewById<TextView>(R.id.dailyCalories)
+                val pro = holder.itemView.findViewById<TextView>(R.id.dailyProtein)
+                val car = holder.itemView.findViewById<TextView>(R.id.dailyCarbs)
+                val fa = holder.itemView.findViewById<TextView>(R.id.dailyFat)
+
+                cal.text = calories.toString()
+                pro.text = protein.toString()
+                car.text = carbohydrate.toString()
+                fa.text = fat.toString()
             }
         }
     }
