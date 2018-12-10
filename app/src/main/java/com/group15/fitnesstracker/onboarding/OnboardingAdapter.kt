5329fa99664481a08b07cf44a9ca96b45a4286b9
awave1 <artem.golovin@renoworks.com>
2018-12-09 23:46:54 -0700

    fix missing fragment after logout

--- app/src/main/java/com/group15/fitnesstracker/onboarding/OnboardingAdapter.kt
+++ app/src/main/java/com/group15/fitnesstracker/onboarding/OnboardingAdapter.kt
@@ -1,9 +1,9 @@
 package com.group15.fitnesstracker.onboarding
 
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 
-class OnboardingAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+class OnboardingAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
     private val PAGES = 3
 
     override fun getItem(position: Int) = OnboardingPageFragment.newInstance(position)
